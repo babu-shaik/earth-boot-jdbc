16bcfe2e1c1515eaeed77833f4a6256eb46c0ced
babu-shaik <babu20.c2s@gmil.com>
2020-06-24 16:41:17 +0530

    initial commit

---
+++ src/main/java/com/example/demo/EarthBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthBootJdbcApplication.class, args);
+	}
+
+}
